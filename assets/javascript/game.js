969e5191c41c1da5bdc35042d54709ea51e35e09
Emma Stotz <stotz.emma@gmail.com>
2019-09-24 16:53:13 -0400

    Updated all of the functions. Need to debug win condition and restart function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,30 +2,37 @@ $(document).ready(function(){
   // GLOBAL VARIABLES ====================================================
   var wins = 0;
   var losses = 0;
-  var userPlay = true;
+  var playerScore = 0;
   var randomNumber;
-  var randomGemNumber;
+  var randomGemNum;
 
   // FUNCTIONS ===========================================================
-  //Assigns a random number for the user to match.
+  //Restart Function
+  var restart = function(){
+    $("#total-score").empty();
+    $("#gem-display").empty();
+  }
+  
+  // Random Number to Match Function
   var setRandomNumber = function(){
     for (i = 0; i < 1; i++){
-      randomNumber = Math.floor(Math.random() * 102 + 19);
-      $("#random-number").text(randomNumber);
+      var randomNum = Math.floor(Math.random() * 102 + 19);
+      $("#random-number").text(randomNum);
+      return randomNum
     }
   };
 
-  // Assigns random numbers to the gems and prints the images to the correct box
+  // Random Gem Numbers Function
   var setGemNumber = function(){
     for (i = 0; i < 4; i++){
       randomGemNumber = Math.floor(Math.random() * 12 + 1);
 
-      var gemImage = ["../images/gems_01.png","../images/gems_02.png","../images/gems_03.png","../images/gems_04.png"];
+      var gemImage = ["assets/images/gems_01.png","assets/images/gems_02.png","assets/images/gems_03.png","assets/images/gems_04.png"];
 
       var gem = $("<img>");
 
       gem.attr({
-        "class": 'gemImage',
+        "class": 'gem-image',
         "numberRandom": randomGemNumber,
         "src": gemImage[i],
       });
@@ -34,11 +41,40 @@ $(document).ready(function(){
     }
   };
 
-  //BEGIN GAME ===========================================================
-  var playFunction = function(){
-    if (userPlay === true){
-      setGemNumber();
-      setRandomNumber();   
+  //Update Player Score Function
+  var updatePlayerScore = function(gemClicked){
+    var gemNumber = parseInt(gemClicked.attr("numberRandom"));
+    playerScore = playerScore + gemNumber;
+    $("#total-score").text(playerScore);
+  }
+
+  //Check Score Function
+  var checkScore = function (){
+    //Winning Condition
+    if (playerScore == randomNumber){
+      win++;
+      alert("You win!");
+      $("#wins").text("Wins: " + wins);
+      restart();
+    }
+    //Losing Condition
+    if (playerScore > randomNumber) {
+      losses++;
+      alert("You lose!");
+      $("#losses").text("Losses: " + losses);
+      restart();
     }
   }
-});
+
+  //BEGIN GAME ===========================================================
+    randomNumber = setRandomNumber();
+    randomGemNum = setGemNumber();
+    $("#wins").text("Wins: " + wins);
+    $("#losses").text("Losses: " + losses);
+
+    //Creating a click event on gem images
+    $(".gem-image").on("click", function() {
+      updatePlayerScore($(this));
+      checkScore();
+    }); 
+  });
